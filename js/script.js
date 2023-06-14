223a84df31f775efeb845bae2e9597ec755f1fc3
austin <124389297+austin-scavone@users.noreply.github.com>
2023-06-14 19:30:21 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan Lee
+// Created on: dec 2022
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-6-02/sw.js", {
+    scope: "/ICS2O-6-02/",
+  })
+}
+
+function cookiePoint() {
+  if (localStorage.howManyTimesClickedTheCookie) {
+    localStorage.howManyTimesClickedTheCookie =
+      Number(localStorage.howManyTimesClickedTheCookie) + 1
+  } else {
+    localStorage.howManyTimesClickedTheCookie = 1
+  }
+  document.getElementById("hits").innerHTML =
+    "Total Hits :" + localStorage.howManyTimesClickedTheCookie
+}
